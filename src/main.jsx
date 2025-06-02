fafa36879c8ef2c0c15263cbc1eb3de97ee029ec
SHAMIKA <RikaXmd@gmail.com>
2025-06-02 23:00:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import RikaChatMaker from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import RikaChatMaker from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RikaChatMaker />
   </React.StrictMode>
-);
+)
