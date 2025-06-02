bb5be834503cf17528db96063f0356c6e346cc2e
SHAMIKA <RikaXmd@gmail.com>
2025-06-02 22:28:30 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import RikaChatMaker from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RikaChatMaker />
+  </React.StrictMode>
+);
